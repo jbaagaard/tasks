75d7184dcbf86fb3be30e92e9c92f855696f0587
Jens Birkbak Aagaard <teamtunfiske@gmail.com>
2023-02-23 14:33:37 +0100

    Update theme.ts

--- styles/theme.ts
+++ styles/theme.ts
@@ -13,7 +13,7 @@ interface Theme {
   green: string;
 }
 
-export const dark: Theme = {
+export const train: Theme = {
   bg: "#1d2021",
   bgDark: "#161819",
   bgLight: "#282828",
@@ -21,17 +21,17 @@ export const dark: Theme = {
   textSubtle: "#897f73",
   textSubtler: "#897f73",
   green: "#a9b665",
-  greenText: "white",
+  greenText: "#d4be98",
   red: "#ea6962",
-  redText: "white",
+  redText: "#d4be98",
   yellow: "#d8a657",
-  yellowText: "white",
+  yellowText: "#d4be98",
 };
 
 export const eightOOEight: Theme = {
   greenText: "#f9fae6",
-  redText: "white",
-  yellowText: "white",
+  redText: "#f9fae6",
+  yellowText: "#f9fae6",
   bg: "#333a45",
   bgDark: "#2e343d",
   bgLight: "#464f5d",
